bfe230dc6a899815d7cb396148730cbaf4ef6754
Igo Saldanha <contato@igosaldanha.com>
2025-02-21 06:57:15 -0300

    Adicionado pagina e formulario para cadastro de novo imovel

---
+++ src/js/new-imovel.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+    $('.cep-mask').mask('00000-000');
+    $('.whatsapp-mask').mask('(00) 00000-0000');
+    $('.rent-mask').mask('R$ 000.000,00', { reverse: true });
+});
